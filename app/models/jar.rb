22a180e11da99f7229d7431a4fe4bbb58eeea85c
Greg <gjean011@fiu.edu>
2013-08-24 17:29:23 -0700

    second try

--- app/models/jar.rb
+++ app/models/jar.rb
@@ -1,5 +1,5 @@
 class Jar < ActiveRecord::Base
-	validates :name, :description, :user_id, :amount1, :amount2, :amount3, :amount4 presence: true
+	validates :name, :description, :user_id, :amount1, :amount2, :amount3, :amount4, presence: true
 
 	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	validates :email, uniqueness: true
-  	validates :email, :first_name, :middle_name, :last_name, :paypal_id presence: true
+  	validates :email, :first_name, :middle_name, :last_name, :paypal_id, presence: true
 
   	has_many :jars
 end
